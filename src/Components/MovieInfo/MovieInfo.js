13484f99435598abb04d59152d5f58701b0998c3
lschnell8 <lschnell8@gmail.com>
2022-02-05 02:50:39 -0700

    Update styling

--- src/Components/MovieInfo/MovieInfo.js
+++ src/Components/MovieInfo/MovieInfo.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './MovieInfo.css';
 
 const MovieInfo = ({ movie, navigateHome }) => {
-  console.log(movie.release_date.split('-'))
+  let movieDate = movie.release_date.split('-');
+  movieDate = [movieDate[1], movieDate[2], movieDate[0]].join('-');
   return (
     <section className='movie-info'>
       <img src={movie.backdrop_path} className='backdrop'/>
       <img src={movie.poster_path} className='cover'/>
       <article className='descriptors'>
         <h1>{movie.title}</h1>
-        <p>{Math.round(movie.average_rating)}  ⭐️</p>
-        <p>Release Date: {movie.release_date}</p>
+        <p>{movie.average_rating.toFixed(1)}  ⭐️</p>
+        <p>Release Date: {movieDate}</p>
         <button onClick={navigateHome}>Home</button>
       </article>
     </section>
