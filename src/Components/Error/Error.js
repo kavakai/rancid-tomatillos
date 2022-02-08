e431f02a985e7cbdf806273d42e7391a23ee785a
lschnell8 <lschnell8@gmail.com>
2022-02-07 18:50:48 -0700

    Add switch statement

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -2,9 +2,17 @@ import React from 'react'
 import './Error.css'
 
 const Error = ({ error, navigateHome }) => {
+  let message = ''
+  switch (error.code) {
+    case error.code >= 400 && error.code < 500:
+      message = "Where is that file?... Sorry. Refresh page and try again"
+      break;
+    case error.code >= 500: 
+      message = "Oops, our server is napping... Refresh page and try again"
+    }
   return (
     <div className="error-container">
-      <h2 className="error">Something went wrong... Refresh page and try again.</h2>
+      <h2 className="error">{message}</h2>
       <br />
       <details className="error">{error.message}</details>
       <button className="errorHome" onClick={navigateHome}>Home</button>
