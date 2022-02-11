51bb4f374b2ba54e43e16d201b0ac01cc3438d3b
Kai Kuller <kavakai@gmail.com>
2022-02-10 20:04:44 -0700

    Add single movie view test file

---
+++ cypress/integration/single-view_spec.js
@@ -0,0 +1,43 @@
+describe('MovieInfo view', () => {
+  
+  beforeEach(() => {
+    let sadMovie = {
+      id: 737173,
+      title: 'Maratón After',
+      poster_path: "https://image.tmdb.org/t/p/original//opZKcgocttEOAUzqluX3bUbbDew.jpg",
+      backdrop_path: "https://www.esm.rochester.edu/uploads/NoPhotoAvailable.jpg",
+      release_date: "2020-09-03",
+      overview: "",
+      genres: [],
+      budget: 0,
+      revenue: 0,
+      runtime: 0,
+      tagline: "",
+      average_rating: 4.333333333333333
+    }
+    let movie = {
+      id: 340102,
+      title: "The New Mutants",
+      poster_path:
+        "https://image.tmdb.org/t/p/original//xrI4EnZWftpo1B7tTvlMUXVOikd.jpg",
+      backdrop_path:
+        "https://image.tmdb.org/t/p/original//eCIvqa3QVCx6H09bdeOS8Al2Sqy.jpg",
+      release_date: "2020-08-26",
+      overview:
+        "Five young mutants, just discovering their abilities while held in a secret facility against their will, fight to escape their past sins and save themselves.",
+      genres: ["Action", "Science Fiction", "Horror", "Adventure"],
+      budget: 67000000,
+      revenue: 3100000,
+      runtime: 94,
+      tagline: "It's time to face your demons",
+      average_rating: 4,
+    };
+
+    it('should display a single movie when a user clicks on a movie poster', () => {
+      cy.visit("http://localhost:3000")
+        .intercept(movie)
+        .contains('h2', movie.title)
+    })
+    
+  });
+})
