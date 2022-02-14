2d1c1dce79da6ba8360e185d6bbc70885ee5eefb
lschnell8 <lschnell8@gmail.com>
2022-02-14 13:51:04 -0700

    Refacator filterMovies method to reduce redundancy

--- src/App.js
+++ src/App.js
@@ -19,72 +19,42 @@ class App extends Component{
       error: '',
       isError: false,
       loading: true,
-      filteredMovies: '',
+      filteredMovies: ''
     }
   }
 
   componentDidMount = () => {
     fetchApi("movies")
       .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else { 
-        this.setState({ error: response.status })
-      } 
+        if (response.ok) {
+          return response.json();
+        } else { 
+          this.setState({ error: response.status })
+        } 
       })
       .then((data) => this.setState({ movies: data.movies, loading: false }))
-      .catch(() => this.setState({ isError: true })
-    );
+      .catch(() => this.setState({ isError: true }))
   }
 
   navigateHome = () => {
     this.setState({filteredMovies: ''})
   }
 
-  filterMovies = ({textInput, dateInput, ratingInput}) => {
-    // console.log('firing')
-    // console.log('FBT', ratingInput)
+  filterMovies = ({ textInput, dateInput, ratingInput }) => {
     let text = textInput.toLowerCase();
-    let filtered;
-    if (textInput.length) { 
-      filtered = this.state.movies.filter(movie => movie.title.toLowerCase().includes(text))
-      filtered.length > 0 ? this.setState({ filteredMovies: [ ...filtered ] }) : this.setState({error: "No movies found, try again"})
-    } else if (dateInput.length > 0) {
-      filtered = this.state.movies.filter(movie => {
-        let compareDate = movie.release_date.split('-').splice(0, 2).join('-');
-        if (compareDate === dateInput) {
-          return movie
-        } 
-      })
-      filtered.length > 0
-        ? this.setState({ filteredMovies: [...filtered] })
-        : this.setState({ error: "No movies found, try again" });
-    } else if (ratingInput.length) {
-      filtered = this.state.movies.filter(movie => {
-        let rating = movie.average_rating / 2
-        if (rating >= ratingInput) {
-          return movie
-        }
-      })
-      filtered.length > 0
-        ? this.setState({ filteredMovies: [...filtered] })
-        : this.setState({ error: "No movies found, try again" }); 
-    }
+    let filtered = this.state.movies.filter(movie => {
+      let compareDate = movie.release_date.split('-').splice(0, 2).join('-');
+      let rating = movie.average_rating / 2;
+      if ((textInput.length && movie.title.toLowerCase().includes(text)) || (dateInput.length > 0 && compareDate === dateInput) || (ratingInput.length && rating >= ratingInput)) {
+        return movie
+      }
+    })
+    filtered.length > 0 ? this.setState({ filteredMovies: [...filtered] }) : this.setState({ error: "No movies found, try again" });
   }
 
   clearFiltered = () => {
     this.setState({filteredMovies: '', error: ''})
   }
-  // filterMovies = (input) => {
-  //   let filtered = this.state.movies.filter(movie => {
-  //     if (input === movie.average_rating.toString() || movie.release_date) {
-  //       this.setState({ filteredMovies: { ...filtered } })
-  //     } else {
-  //       this.setState({filteredMovies: 'Sorry, there are no matches'})
-  //     }
-  //   })
-  // }
-  
 
   render() {
     if (this.state.loading && !this.state.error) {
@@ -94,24 +64,18 @@ class App extends Component{
         <main>
           <Switch>
             <Route exact path="/"
-              render={() => (
-                this.state.error
-                  ? <Error
-                    error={this.state.error}
-                    clearFiltered={this.clearFiltered}
-                  />
-                  :
+              render={() =>
+              (this.state.error ?
+                <Error error={this.state.error} clearFiltered={this.clearFiltered} /> :
                 <>
-                    <Header />
-                    {/* <section className='main-display'> */}
-                    <Sidebar filterMovies={this.filterMovies} clearFiltered={this.clearFiltered}/>
-                    <MovieContainer
+                  <Header />
+                  <Sidebar filterMovies={this.filterMovies} clearFiltered={this.clearFiltered} />
+                  <MovieContainer
                     navigateHome={this.navigateHome}
                     filteredMovies={this.state.filteredMovies}
                     movies={this.state.movies}
                     selectMovie={this.selectMovie}
                   />
-                    {/* </section> */}
                 </>
               )}
             />
@@ -119,18 +83,15 @@ class App extends Component{
               path="/:id"
               render={({ match }) => {
                 const movieId = parseInt(match.params.id);
-                return (
-                  <MovieInfo id={movieId} navigateHome={this.navigateHome} />
-                );
-              }}
+                return (<MovieInfo id={movieId} navigateHome={this.navigateHome} />);
+                }
+              }
             />
           </Switch>
         </main>
       );
     }
   }
-
-}
-
+};
 
 export default App;
